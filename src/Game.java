f160942993aeb8c98e3891ee2ef4e0706341ab54
ElenJ <elena.jolkver@gmail.com>
2019-06-04 20:27:52 +0200

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,11 @@
+package com.edu4java.minitennis1;
+import javax.swing.JFrame;
+
+public class Game {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Elenas Mini Tennis");
+        frame.setSize(600, 600);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
