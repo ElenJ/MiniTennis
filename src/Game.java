4c481579efe8a81f75bf6b4500344fa93c587491
ElenJ <elena.jolkver@gmail.com>
2019-06-05 22:00:14 +0200

    ball bounces back from canvas

--- src/Game.java
+++ src/Game.java
@@ -1,29 +1,48 @@
-package com.edu4java.minitennis1;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.awt.geom.Ellipse2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+//@SuppressWarnings("serial")
 public class Game extends JPanel {
 
-    //override
-    public void paint(Graphics g){
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(Color.BLUE);
-        g2d.fillOval(0,0,30,30);
-        g2d.drawOval(0,50,30,30);
-        g2d.fillRect(50, 0, 30,30);
-        g2d.drawRect(50,50,30,30);
-        g2d.draw(new Ellipse2D.Double(0,100,30,30));
+    int x = 0;
+    int y = 0;
+    int xa = 1;
+    int ya = 1;
+
+    private void moveBall() {
+        if (x+xa <0){ xa=1;}
+        if (x+xa > getWidth()-30){xa = -1;}
+        if (y+ya <0) {ya = 1;}
+        if (y +ya >getHeight()-30) {ya = -1;}
+        x=x+xa;
+        y=y+ya;
     }
-    public static void main(String[] args) {
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.fillOval(x, y, 30, 30);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         JFrame frame = new JFrame("Elenas Mini Tennis");
-        frame.add(new Game());
-        frame.setSize(600, 600);
+        Game game = new Game();
+        frame.add(game);
+        frame.setSize(300, 400);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        while (true) {
+
+            game.repaint();
+            game.moveBall();
+            Thread.sleep(10);
+        }
     }
 }
