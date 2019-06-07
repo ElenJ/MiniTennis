25ad655ca7a49ac6125be2355621258b55a4da89
ElenJ <elena.jolkver@gmail.com>
2019-06-07 23:02:27 +0200

    ball bounces back from canvas ball is an own class
    Signed-off-by: ElenJ <elena.jolkver@gmail.com>

--- src/Ball.java
+++ src/Ball.java
@@ -1,6 +1,7 @@
-import java.awt.Graphics2D;
+import java.awt.*;
 
 public class Ball {
+    private static final int DIAMETER=30;
     int x = 0;
     int y = 0;
     int xa = 1;
@@ -12,20 +13,36 @@ public class Ball {
     }
 
     void move() {
+        boolean changeDirection=true;
         if (x + xa < 0)
-            xa = 1;
-        if (x + xa > game.getWidth() - 30)
-            xa = -1;
-        if (y + ya < 0)
-            ya = 1;
-        if (y + ya > game.getHeight() - 30)
-            ya = -1;
+            xa = game.getSpeed();
+        else if (x + xa > game.getWidth() - DIAMETER)
+            xa = -game.getSpeed();
+        else if (y + ya < 0)
+            ya = game.getSpeed();
+        else if (y + ya > game.getHeight() - DIAMETER)
+            game.gameOver();
+        else if (collision()){
+            ya = -game.getSpeed();
+            y = game.racquet.getTopY() - DIAMETER;
+            game.speedIncrease();
+        } else changeDirection=false;
 
+        if (changeDirection)
+            SoundEffect.HIT.play();
         x = x + xa;
         y = y + ya;
     }
 
+    private boolean collision(){
+        return game.racquet.getBounds().intersects(getBounds());
+    }
+
     public void paint(Graphics2D g) {
+        g.setColor(Color.GREEN);
         g.fillOval(x, y, 30, 30);
+    }
+    public Rectangle getBounds(){
+        return new Rectangle(x,y,DIAMETER, DIAMETER);
     }
 }
--- src/Game2.java
+++ src/Game2.java
@@ -1,16 +1,61 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Font;
+
 
 @SuppressWarnings("serial")
 public class Game2 extends JPanel {
 
     Ball ball = new Ball(this);
+    Racquet racquet = new Racquet(this);
+   // int speed = 1;
+    private int speed = 1;
+
+    public int getSpeed(){
+
+        return speed;
+    }
+
+    public void speedIncrease(){
+
+        speed=speed+1;
+    }
+
+    private int getScore() {
+        return speed -1;
+    }
+
+
+    public Game2() {
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                racquet.keyPressed(e);
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                racquet.keyReleased(e);
+            }
+        });
+        setFocusable(true);
+       // Sound.BACK.loop();
+    }
 
     private void move() {
         ball.move();
+        racquet.move();
     }
 
     @Override
@@ -20,9 +65,19 @@ public class Game2 extends JPanel {
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
         ball.paint(g2d);
+        racquet.paint(g2d);
+        g2d.setColor(Color.GRAY);
+        g2d.setFont(new Font("Verdana", Font.BOLD, 30));
+        g2d.drawString(String.valueOf(getScore()), 10, 30);
 
     }
 
+    public void gameOver(){
+        SoundEffect.BACKGROUND.stop();
+        SoundEffect.GAMEOVER.play();
+        JOptionPane.showMessageDialog(this, "your score is: " + getScore(),"game over", JOptionPane.YES_NO_OPTION);
+        System.exit(ABORT);
+    }
     public static void main(String[] args) throws InterruptedException {
         JFrame frame = new JFrame("Mini Tennis");
         Game2 game = new Game2();
@@ -31,10 +86,13 @@ public class Game2 extends JPanel {
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
         while (true) {
+            SoundEffect.BACKGROUND.soundloop();
             game.move();
             game.repaint();
             Thread.sleep(10);
         }
     }
+
 }
